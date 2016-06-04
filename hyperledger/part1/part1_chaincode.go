0223ba4bfa1e3c16252a83953330235a406f02d5
raphael-hoed <raphael.hoed@gmail.com>
2016-06-04 15:27:15 -0300

    Alterações no chaincode

--- hyperledger/part1/part1_chaincode.go
+++ hyperledger/part1/part1_chaincode.go
@@ -108,7 +108,7 @@ func (t *SimpleChaincode) Invoke(stub *shim.ChaincodeStub, function string, args
 	} else if function == "write" {											//writes a value to the chaincode state
 		return t.Write(stub, args)
 	} else if function == "init_marble" {									//create a new marble
-		return t.init_marble(stub, args)
+		return t.init_IFNMG(stub, args)
 	} else if function == "set_user" {										//change owner of a marble
 		return t.set_user(stub, args)
 	}
